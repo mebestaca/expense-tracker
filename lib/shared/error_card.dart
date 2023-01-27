5bf0de2159061a2c08eea995ce6704142a9c7491
mebestaca <mebestaca@gmail.com>
2023-01-26 18:31:30 -0700

    added error_card

---
+++ lib/shared/error_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ErrorCard extends StatelessWidget {
+  const ErrorCard({Key? key, required this.errorText}) : super(key: key);
+
+  final String errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).errorColor,
+          ),
+        ),
+        child: ListTile(
+          title: Text(errorText,
+            style: TextStyle(
+              color: Theme.of(context).errorColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
