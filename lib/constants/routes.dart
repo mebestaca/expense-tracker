5a355e1b27aa3f3ed34245d594bc655e9739da89
mebestaca <mebestaca@gmail.com>
2023-01-26 19:34:30 -0700

    added constant routes

---
+++ lib/constants/routes.dart
@@ -0,0 +1,7 @@
+class Routes{
+  static const String dashboard = "/dashboard";
+  static const String lowItems = "/lowItems";
+  static const String allItems = "/allItems";
+  static const String maintenance = "/maintenance";
+  static const String sharedScaffold = "/addItem";
+}
