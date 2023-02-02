bf7d6133ca92e99c2113c863dfff1f05b80049c7
mebestaca <mebestaca@gmail.com>
2023-02-02 05:28:30 -0700

    removed uid

--- lib/models/model_category.dart
+++ lib/models/model_category.dart
@@ -4,18 +4,15 @@ class CategoryModel{
 
   CategoryModel({this.uid = "", this.category = ""});
 
-  static const String fieldUID = "UID";
   static const String fieldCATEGORY = "CATEGORY";
 
   CategoryModel.fromJson(Map<String, Object?> json):
       this(
-        uid : json[fieldUID] as String,
         category : json[fieldCATEGORY] as String
       );
 
   Map<String, Object?> toJson() {
     return {
-      fieldUID : uid,
       fieldCATEGORY : category
     };
   }
