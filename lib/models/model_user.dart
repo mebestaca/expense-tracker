d2a9ee0bfc4ee2888a609d4920c34b35e8e03308
mebestaca <mebestaca@gmail.com>
2023-02-02 03:16:52 -0700

    added converter to user model

--- lib/models/model_user.dart
+++ lib/models/model_user.dart
@@ -11,4 +11,22 @@ class UserModel{
   static const String fieldLASTNAME = "LAST_NAME";
   static const String fieldID = "ID";
 
+
+  UserModel.fromJson(Map<String, Object?> json) :
+        this(
+          uid: json[fieldUID] as String,
+          firstName: json[fieldFIRSTNAME] as String,
+          lastName: json[fieldLASTNAME] as String,
+          id: json[fieldID] as String
+      );
+
+  Map<String, Object?> toJson() {
+    return {
+      fieldUID : uid,
+      fieldFIRSTNAME : firstName,
+      fieldLASTNAME : lastName,
+      fieldID : id
+    };
+  }
+
 }
