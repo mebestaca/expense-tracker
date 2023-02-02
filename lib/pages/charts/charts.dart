7145c0aebc7a9e272ac69e64692ee4bb32e4c82c
mebestaca <mebestaca@gmail.com>
2023-02-02 03:22:08 -0700

    added placeholder widget

---
+++ lib/pages/charts/charts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Charts extends StatefulWidget {
+  const Charts({Key? key}) : super(key: key);
+
+  @override
+  State<Charts> createState() => _ChartsState();
+}
+
+class _ChartsState extends State<Charts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("asd");
+  }
+}
