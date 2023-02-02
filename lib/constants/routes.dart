c77b560d025b17517bf963a1ffd412ae07e2e495
mebestaca <mebestaca@gmail.com>
2023-02-02 03:13:04 -0700

    changed routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,7 +1,3 @@
 class Routes{
-  static const String dashboard = "/dashboard";
-  static const String lowItems = "/lowItems";
-  static const String allItems = "/allItems";
-  static const String maintenance = "/maintenance";
-  static const String sharedScaffold = "/addItem";
+  static const String categoryMaintenance = "/category_maintenance";
 }
