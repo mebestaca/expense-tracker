9e601ca3511407c0a9a94bb39b0a8ce20bf86c3c
mebestaca <mebestaca@gmail.com>
2023-02-02 20:02:16 -0700

    removed all references to other classes

--- lib/shared/widgets/generic_list_tile.dart
+++ lib/shared/widgets/generic_list_tile.dart
@@ -1,17 +1,15 @@
-import 'package:expense_tracker/constants/routes.dart';
-import 'package:expense_tracker/models/model_category.dart';
 import 'package:expense_tracker/shared/widgets/category_data_entry.dart';
 import 'package:expense_tracker/shared/widgets/generic_popup_menu_button.dart';
 import 'package:flutter/material.dart';
 
-import '../../services/database.dart';
-
 class GenericListTile extends StatefulWidget {
-  const GenericListTile({Key? key, required this.id, required this.model, required this.path}) : super(key: key);
+  const GenericListTile({Key? key, required this.id, required this.path, required this.title, required this.switchFunction, required this.popUpMenuItemList}) : super(key: key);
 
   final String id;
   final String path;
-  final CategoryModel model;
+  final String title;
+  final Function switchFunction;
+  final List<PopupMenuItem> popUpMenuItemList;
 
   @override
   State<GenericListTile> createState() => _GenericListTileState();
@@ -34,38 +32,12 @@ class _GenericListTileState extends State<GenericListTile> {
             side: const BorderSide(color: Colors.black, width: 1),
             borderRadius: BorderRadius.circular(5),
           ),
-        title: Text(widget.model.category),
+        title: Text(widget.title),
         trailing: GenericPopUpMenuButton(
           switchFunction: (item) async {
-            switch(item) {
-              case CategoryEntryMode.add:
-                break;
-              case CategoryEntryMode.edit:
-                Navigator.pushNamed(context, Routes.categoryMaintenance,
-                    arguments: {
-                      "widget" : CategoryDataEntry(
-                        entryMode: CategoryEntryMode.edit,
-                        id: "",
-                        model: CategoryModel(),
-                        path: widget.path,
-                      ),
-                      "title" : "New Category",
-                    });
-                break;
-              case CategoryEntryMode.delete:
-                await DatabaseService(path: widget.path).deleteEntry(widget.id);
-            }
+            widget.switchFunction(item);
           },
-          popUpMenuItemList: const [
-            PopupMenuItem<CategoryEntryMode>(
-                value: CategoryEntryMode.edit,
-                child: Text("Edit")
-            ),
-            PopupMenuItem<CategoryEntryMode>(
-                value: CategoryEntryMode.delete,
-                child: Text("Delete")
-            ),
-          ],
+          popUpMenuItemList: widget.popUpMenuItemList,
         ),
       ),
     );
