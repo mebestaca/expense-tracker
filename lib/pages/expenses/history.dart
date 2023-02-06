a49596d2bd1a0bffff7f7483e78b71a69a7990b7
mebestaca <mebestaca@gmail.com>
2023-02-05 18:28:41 -0700

    added temporary history page

---
+++ lib/pages/expenses/history.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistoryExpenses extends StatefulWidget {
+  const HistoryExpenses({Key? key}) : super(key: key);
+
+  @override
+  State<HistoryExpenses> createState() => _HistoryExpensesState();
+}
+
+class _HistoryExpensesState extends State<HistoryExpenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
