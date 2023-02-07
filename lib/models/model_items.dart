cab0277d5ac9cbac188d620b1912c0aa926a8a87
mebestaca <mebestaca@gmail.com>
2023-02-06 18:05:28 -0700

    modified item model

--- lib/models/model_items.dart
+++ lib/models/model_items.dart
@@ -1,14 +1,35 @@
 class ItemModel {
   final String uid;
-  final String id;
   final String name;
   final String category;
+  final String transDate;
+  final double amount;
 
-  ItemModel(this.uid, this.id, this.name, this.category);
+  ItemModel({this.amount=0, this.transDate="", this.uid="", this.name="", this.category=""});
 
   static const String fieldUid = "UID";
-  static const String fieldId = "ID";
   static const String fieldName = "ITEM_NAME";
   static const String fieldCategory = "CATEGORY";
+  static const String fieldDate = "TRANS_DATE";
+  static const String fieldAmount = "AMOUNT";
+
+  ItemModel.fromJson(Map<String, Object?> json) :
+        this(
+          uid: json[fieldUid] as String,
+          name: json[fieldName] as String,
+          category: json[fieldCategory] as String,
+          transDate: json[fieldDate] as String,
+          amount: json[fieldAmount] as double
+      );
+
+  Map<String, Object?> toJson() {
+    return {
+      fieldUid : uid,
+      fieldName : name,
+      fieldCategory : category,
+      fieldDate : transDate,
+      fieldAmount : amount,
+    };
+  }
 
 }
