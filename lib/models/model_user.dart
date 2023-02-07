80171eae073b41002a3eaba4fa2548c5720ca082
mebestaca <mebestaca@gmail.com>
2023-02-06 18:04:15 -0700

    removed id

--- lib/models/model_user.dart
+++ lib/models/model_user.dart
@@ -2,9 +2,8 @@ class UserModel{
   final String uid;
   final String firstName;
   final String lastName;
-  final String id;
 
-  UserModel({this.uid="", this.firstName="", this.lastName="", this.id=""});
+  UserModel({this.uid="", this.firstName="", this.lastName=""});
 
   static const String fieldUID = "UID";
   static const String fieldFIRSTNAME = "FIRST_NAME";
@@ -16,16 +15,14 @@ class UserModel{
         this(
           uid: json[fieldUID] as String,
           firstName: json[fieldFIRSTNAME] as String,
-          lastName: json[fieldLASTNAME] as String,
-          id: json[fieldID] as String
+          lastName: json[fieldLASTNAME] as String
       );
 
   Map<String, Object?> toJson() {
     return {
       fieldUID : uid,
       fieldFIRSTNAME : firstName,
-      fieldLASTNAME : lastName,
-      fieldID : id
+      fieldLASTNAME : lastName
     };
   }
 
