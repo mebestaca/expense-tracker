f0782e71316e14679158a430a64419335385cb07
mebestaca <mebestaca@gmail.com>
2023-02-07 19:02:35 -0700

    added a temporary expenses data entry page

--- lib/shared/widgets/today_data_entry.dart
+++ lib/shared/widgets/today_data_entry.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../models/model_items.dart';
+import '../text_decoration.dart';
 
 enum ExpenseEntryMode{
   add,
@@ -21,8 +22,160 @@ class TodayDataEntry extends StatefulWidget {
 }
 
 class _TodayDataEntryState extends State<TodayDataEntry> {
+
+  late String itemName;
+  late double amount;
+  late String transDate;
+  late String category;
+
+  final itemNameController = TextEditingController();
+  final amountController = TextEditingController();
+  final transDateController = TextEditingController();
+  final categoryController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  bool isInhibited = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    if (widget.entryMode == ExpenseEntryMode.edit) {
+      setState(() {
+        if (!isInhibited) {
+          itemName = widget.model.name;
+          itemNameController.text = itemName;
+          isInhibited = true;
+        }
+      });
+    }
+
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              controller: itemNameController,
+              validator: (val) {
+                return val != null && val.isNotEmpty ? null : "please enter an item name";
+              },
+              decoration: fieldStyle.copyWith(
+                  hintText: "date",
+                  labelText: "date"
+              ),
+              onChanged: (val) {
+                setState(() {
+                  itemName = val;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              controller: itemNameController,
+              validator: (val) {
+                return val != null && val.isNotEmpty ? null : "please enter an item name";
+              },
+              decoration: fieldStyle.copyWith(
+                  hintText: "item name",
+                  labelText: "item name"
+              ),
+              onChanged: (val) {
+                setState(() {
+                  itemName = val;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              controller: itemNameController,
+              validator: (val) {
+                return val != null && val.isNotEmpty ? null : "please enter an amount";
+              },
+              decoration: fieldStyle.copyWith(
+                  hintText: "amount",
+                  labelText: "amount"
+              ),
+              onChanged: (val) {
+                setState(() {
+                  itemName = val;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              controller: itemNameController,
+              validator: (val) {
+                return val != null && val.isNotEmpty ? null : "please enter an item name";
+              },
+              decoration: fieldStyle.copyWith(
+                  hintText: "category",
+                  labelText: "category"
+              ),
+              onChanged: (val) {
+                setState(() {
+                  itemName = val;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()){
+
+                      /*
+                      Map<String, dynamic> data = {
+                        CategoryModel.fieldCATEGORY : categoryName
+                      };
+
+                      final snackBar = SnackBar(
+                        content: Text("$categoryName added"),
+                        action: SnackBarAction(
+                          label: "Ok",
+                          onPressed: () {
+
+                          },
+                        ),
+                      );
+
+                      if (widget.entryMode == CategoryEntryMode.add) {
+                        await DatabaseService(path: widget.path).addEntry(data);
+
+                        setState(() {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          categoryController.clear();
+                          categoryName = "";
+                        });
+                      }
+
+                      if (widget.entryMode == CategoryEntryMode.edit) {
+                        await DatabaseService(path: widget.path).updateEntry(data, widget.id).then((value) {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          Navigator.pop(context);
+                        });
+                      }
+                      */
+                    }
+                  },
+                  child: const Text("Confirm")
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
