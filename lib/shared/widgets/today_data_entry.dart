5e9fdf6bad7346c62fd22129ab2c319de83f46d8
mebestaca <mebestaca@gmail.com>
2023-02-08 21:21:35 -0700

    added temporary date time picker

--- lib/shared/widgets/today_data_entry.dart
+++ lib/shared/widgets/today_data_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../models/model_items.dart';
 import '../text_decoration.dart';
@@ -35,7 +36,18 @@ class _TodayDataEntryState extends State<TodayDataEntry> {
 
   final formKey = GlobalKey<FormState>();
 
+  late DateTime currentDate;
+  final dateFormatter = DateFormat("yyyy-MM-dd");
+
   bool isInhibited = false;
+
+  @override
+  void initState() {
+    currentDate = DateTime.now();
+    transDate = dateFormatter.format(currentDate);
+    transDateController.text = transDate;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,127 +67,155 @@ class _TodayDataEntryState extends State<TodayDataEntry> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              controller: itemNameController,
-              validator: (val) {
-                return val != null && val.isNotEmpty ? null : "please enter an item name";
-              },
-              decoration: fieldStyle.copyWith(
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                readOnly: true,
+                controller: transDateController,
+                validator: (val) {
+                  return val != null && val.isNotEmpty ? null : "please enter an item name";
+                },
+                decoration: fieldStyle.copyWith(
                   hintText: "date",
-                  labelText: "date"
+                  labelText: "date",
+                  suffixIcon: IconButton(
+                      onPressed: () => _selectDate(context),
+                      icon: const Icon(Icons.calendar_month)
+                  )
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    transDate = val;
+                  });
+                },
               ),
-              onChanged: (val) {
-                setState(() {
-                  itemName = val;
-                });
-              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              controller: itemNameController,
-              validator: (val) {
-                return val != null && val.isNotEmpty ? null : "please enter an item name";
-              },
-              decoration: fieldStyle.copyWith(
-                  hintText: "item name",
-                  labelText: "item name"
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: itemNameController,
+                validator: (val) {
+                  return val != null && val.isNotEmpty ? null : "please enter an item name";
+                },
+                decoration: fieldStyle.copyWith(
+                    hintText: "item name",
+                    labelText: "item name"
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    itemName = val;
+                  });
+                },
               ),
-              onChanged: (val) {
-                setState(() {
-                  itemName = val;
-                });
-              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              controller: itemNameController,
-              validator: (val) {
-                return val != null && val.isNotEmpty ? null : "please enter an amount";
-              },
-              decoration: fieldStyle.copyWith(
-                  hintText: "amount",
-                  labelText: "amount"
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: itemNameController,
+                validator: (val) {
+                  return val != null && val.isNotEmpty ? null : "please enter an amount";
+                },
+                decoration: fieldStyle.copyWith(
+                    hintText: "amount",
+                    labelText: "amount"
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    itemName = val;
+                  });
+                },
               ),
-              onChanged: (val) {
-                setState(() {
-                  itemName = val;
-                });
-              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              controller: itemNameController,
-              validator: (val) {
-                return val != null && val.isNotEmpty ? null : "please enter an item name";
-              },
-              decoration: fieldStyle.copyWith(
-                  hintText: "category",
-                  labelText: "category"
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: itemNameController,
+                validator: (val) {
+                  return val != null && val.isNotEmpty ? null : "please enter an item name";
+                },
+                decoration: fieldStyle.copyWith(
+                    hintText: "category",
+                    labelText: "category"
+                ),
+                onChanged: (val) {
+                  setState(() {
+                    itemName = val;
+                  });
+                },
               ),
-              onChanged: (val) {
-                setState(() {
-                  itemName = val;
-                });
-              },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () async {
-                    if (formKey.currentState!.validate()){
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()){
 
-                      /*
-                      Map<String, dynamic> data = {
-                        CategoryModel.fieldCATEGORY : categoryName
-                      };
+                        /*
+                        Map<String, dynamic> data = {
+                          CategoryModel.fieldCATEGORY : categoryName
+                        };
 
-                      final snackBar = SnackBar(
-                        content: Text("$categoryName added"),
-                        action: SnackBarAction(
-                          label: "Ok",
-                          onPressed: () {
+                        final snackBar = SnackBar(
+                          content: Text("$categoryName added"),
+                          action: SnackBarAction(
+                            label: "Ok",
+                            onPressed: () {
 
-                          },
-                        ),
-                      );
+                            },
+                          ),
+                        );
 
-                      if (widget.entryMode == CategoryEntryMode.add) {
-                        await DatabaseService(path: widget.path).addEntry(data);
+                        if (widget.entryMode == CategoryEntryMode.add) {
+                          await DatabaseService(path: widget.path).addEntry(data);
 
-                        setState(() {
-                          FocusManager.instance.primaryFocus?.unfocus();
-                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                          categoryController.clear();
-                          categoryName = "";
-                        });
+                          setState(() {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                            categoryController.clear();
+                            categoryName = "";
+                          });
+                        }
+
+                        if (widget.entryMode == CategoryEntryMode.edit) {
+                          await DatabaseService(path: widget.path).updateEntry(data, widget.id).then((value) {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            Navigator.pop(context);
+                          });
+                        }
+                        */
                       }
-
-                      if (widget.entryMode == CategoryEntryMode.edit) {
-                        await DatabaseService(path: widget.path).updateEntry(data, widget.id).then((value) {
-                          FocusManager.instance.primaryFocus?.unfocus();
-                          Navigator.pop(context);
-                        });
-                      }
-                      */
-                    }
-                  },
-                  child: const Text("Confirm")
+                    },
+                    child: const Text("Confirm")
+                ),
               ),
             ),
           )
         ],
       ),
     );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != currentDate) {
+      setState(() {
+        currentDate = picked;
+      });
+    }
   }
 }
