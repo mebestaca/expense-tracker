1cc9ce4ae3c87a8cd98df692914f1b3df32e8929
mebestaca <mebestaca@gmail.com>
2023-02-09 19:54:32 -0700

    added a future builder that fetches category list before passing it to the data entry to prevent setstate from resetting the entire page and removing the selected item from the selection

--- lib/pages/expenses/today_list.dart
+++ lib/pages/expenses/today_list.dart
@@ -2,6 +2,8 @@ import 'package:expense_tracker/models/model_items.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/routes.dart';
+import '../../services/database.dart';
+import '../../shared/widgets/loading_screen.dart';
 import '../../shared/widgets/today_data_entry.dart';
 
 class TodayList extends StatefulWidget {
@@ -15,59 +17,84 @@ class TodayList extends StatefulWidget {
 
 class _TodayListState extends State<TodayList> {
 
-
-
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-      children: [
-        Container(
-          color: Theme.of(context).canvasColor,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: Container(
-                color: Theme.of(context).primaryColor,
-                child: const Text("0000.00")
-              ),
-            ),
-          ),
-        ),
-        const Expanded(
-          child: Placeholder(),
-        ),
-        Container(
-          color: Theme.of(context).canvasColor,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.genericRoute,
-                        arguments: {
-                          "widget" : TodayDataEntry(
-                            entryMode: ExpenseEntryMode.add,
-                            id: "",
-                            model: ItemModel(),
-                            path: widget.path,
-                          ),
-                          "title" : "New Entry",
-                        }
-                    );
-                  },
-                  child: const Text("Add New Entry",
-                    style: TextStyle(
-                        fontSize: 18
+    return FutureBuilder(
+      future: DatabaseService(path: widget.path).getCategoryModelReference().get(),
+      builder: (context, categories) {
+        if (categories.hasData) {
+          final categoriesData = categories.requireData;
+
+          if (categoriesData.size > 0) {
+
+            List<String> categoriesList = categoriesData.docs.map((docs) {
+              return docs.data().category;
+            }).toList();
+            categoriesList.add("Uncategorized");
+            categoriesList = categoriesList.toSet().toList();
+
+            return Column(
+              children: [
+                Container(
+                  color: Theme.of(context).canvasColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Container(
+                          color: Theme.of(context).primaryColor,
+                          child: const Text("0000.00")
+                      ),
                     ),
-                  )
-              ),
-            ),
-          ),
-        )
-      ],
+                  ),
+                ),
+                const Expanded(
+                  child: Placeholder(),
+                ),
+                Container(
+                  color: Theme.of(context).canvasColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, Routes.genericRoute,
+                                arguments: {
+                                  "widget" : TodayDataEntry(
+                                    entryMode: ExpenseEntryMode.add,
+                                    id: "",
+                                    model: ItemModel(),
+                                    path: widget.path,
+                                    categoryList: categoriesList,
+                                  ),
+                                  "title" : "New Entry",
+                                }
+                            );
+                          },
+                          child: const Text("Add New Entry",
+                            style: TextStyle(
+                                fontSize: 18
+                            ),
+                          )
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          }
+          else {
+            return const Center(
+              child: Text("No data found"),
+            );
+          }
+        }
+        else {
+          return const Loading();
+        }
+      }
     );
   }
 }
