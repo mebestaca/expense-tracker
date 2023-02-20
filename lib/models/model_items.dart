357c1d1fcbdac39af5ebc178f849518a4a4280b6
mebestaca <mebestaca@gmail.com>
2023-02-19 21:02:22 -0700

    month, day and year moved to a split version to easily query using firebase

--- lib/models/model_items.dart
+++ lib/models/model_items.dart
@@ -3,20 +3,29 @@ class ItemModel {
   final String category;
   final String transDate;
   final double amount;
+  final String year;
+  final String month;
+  final String day;
 
-  ItemModel({this.amount=0, this.transDate="", this.name="", this.category=""});
+  ItemModel({this.year="", this.month="", this.day="", this.amount=0, this.transDate="", this.name="", this.category=""});
 
   static const String fieldName = "ITEM_NAME";
   static const String fieldCategory = "CATEGORY";
   static const String fieldDate = "TRANS_DATE";
   static const String fieldAmount = "AMOUNT";
+  static const String fieldYear = "YEAR";
+  static const String fieldMonth = "MONTH";
+  static const String fieldDay = "DAY";
 
   ItemModel.fromJson(Map<String, Object?> json) :
         this(
           name: json[fieldName] as String,
           category: json[fieldCategory] as String,
           transDate: json[fieldDate] as String,
-          amount: json[fieldAmount] as double
+          amount: json[fieldAmount] as double,
+          year : json[fieldYear] as String,
+          month : json[fieldMonth] as String,
+          day : json[fieldDay] as String
       );
 
   Map<String, Object?> toJson() {
@@ -25,6 +34,9 @@ class ItemModel {
       fieldCategory : category,
       fieldDate : transDate,
       fieldAmount : amount,
+      fieldYear : year,
+      fieldMonth : month,
+      fieldDay : day
     };
   }
 
