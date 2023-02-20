86af5e8023983ef8ae7d7d73361cfb7b414c3815
mebestaca <mebestaca@gmail.com>
2023-02-19 21:03:11 -0700

    renamed historyexpenses to historylist

---
+++ lib/pages/expenses/history_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistoryList extends StatefulWidget {
+  const HistoryList({Key? key}) : super(key: key);
+
+  @override
+  State<HistoryList> createState() => _HistoryListState();
+}
+
+class _HistoryListState extends State<HistoryList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
