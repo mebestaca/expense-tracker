e2e692a7bf751803e02b329479a6a1353f9b060d
mebestaca <mebestaca@gmail.com>
2023-02-24 18:33:17 -0700

    added temporary chart template

--- lib/pages/charts/charts.dart
+++ lib/pages/charts/charts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Charts extends StatefulWidget {
   const Charts({Key? key}) : super(key: key);
@@ -8,8 +9,93 @@ class Charts extends StatefulWidget {
 }
 
 class _ChartsState extends State<Charts> {
+
+  late List<_ChartData> data;
+
+  @override
+  void initState() {
+    data = [
+      _ChartData('David', 25),
+      _ChartData('Steve', 38),
+      _ChartData('Jack', 34),
+      _ChartData('Others', 52)
+    ];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("asd");
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ExpansionTile(
+            title: const Text("Today"),
+            children: [
+              const Divider(
+                height: 10.0,
+              ),
+              SfCircularChart(
+                annotations: [
+                  CircularChartAnnotation(
+                    widget: const Text("Categories")
+                  )
+                ],
+                legend: Legend(
+                  isVisible: true,
+                ),
+                series: <CircularSeries<_ChartData, String>>[
+                  DoughnutSeries<_ChartData, String>(
+                    dataSource: data,
+                    xValueMapper: (_ChartData data, _) => data.x,
+                    yValueMapper: (_ChartData data, _) => data.y,
+                    dataLabelSettings: const DataLabelSettings(isVisible: true),
+                  explode: true,
+                  explodeAll: true,
+                  explodeOffset: "3"),
+
+                ]
+              )
+            ],
+          ),
+          ExpansionTile(
+            title: const Text("This month"),
+            children: [
+              SfCircularChart(
+                  series: <CircularSeries<_ChartData, String>>[
+                    DoughnutSeries<_ChartData, String>(
+                        dataSource: data,
+                        xValueMapper: (_ChartData data, _) => data.x,
+                        yValueMapper: (_ChartData data, _) => data.y,
+                        name: "Gold"
+                    ),
+                  ]
+              )
+            ],
+          ),
+          ExpansionTile(
+            title: const Text("This year"),
+            children: [
+              SfCircularChart(
+                series: <CircularSeries<_ChartData, String>>[
+                    DoughnutSeries<_ChartData, String>(
+                      dataSource: data,
+                      xValueMapper: (_ChartData data, _) => data.x,
+                      yValueMapper: (_ChartData data, _) => data.y,
+                      name: "Gold"
+                    ),
+                  ]
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y);
+
+  final String x;
+  final double y;
 }
