b0dccd682cf49247ec2c062e6943828013b4f451
mebestaca <mebestaca@gmail.com>
2023-02-27 18:09:46 -0700

    added chart model that will help display chart data

---
+++ lib/models/model_chart.dart
@@ -0,0 +1,10 @@
+class ChartData {
+  final String name;
+  final double amount;
+
+  ChartData({this.name="", this.amount=0});
+
+  static const String fieldName = "ITEM_NAME";
+  static const String fieldAmount = "AMOUNT";
+
+}
